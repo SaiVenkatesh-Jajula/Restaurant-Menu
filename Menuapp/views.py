db42c2d991aefb9c86f1165968172b55b7205511
saivenkatesh <saivenkatesh619@gmail.com>
2024-03-11 00:56:45 +0530

    Created Views & templates and urls configured

--- Menuapp/views.py
+++ Menuapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Menu
 
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+class MenuList(generic.ListView):
+    # A variable which will store list of data
+    queryset = Menu.objects.order_by("dish_name")
+    template_name = "index.html"
+
+
+class DishDetails(generic.DetailView):
+    model = Menu
+    template_name = "dish_details.html"
